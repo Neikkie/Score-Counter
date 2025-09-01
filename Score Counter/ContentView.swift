f9009a6cb2a53c965facd896b4794b896197c2d1
Neikkie <neikkie21@outlook.com>
2025-09-01 14:48:00 -0400

    feat: created score counter

--- Score Counter/ContentView.swift
+++ Score Counter/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var score: Int = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationStack {
+            VStack {
+                Text(String(score))
+                    .font(.title2)
+                
+                Image("patriots")
+                    .resizable()
+                    .frame(width: 300, height: 300)
+                
+                Button("Score") {
+                    score = score + 1
+                }
+                .buttonStyle(.borderedProminent)
+            }
+            .navigationTitle("Score Counter")
         }
-        .padding()
     }
 }
 
