3b237d05cf10223b53dbc1c8779f73c8e7d84ede
Neikkie <neikkie21@outlook.com>
2025-09-01 14:13:09 -0400

    Initial Commit

---
+++ Score Counter/Score_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Score_CounterApp.swift
+//  Score Counter
+//
+//  Created by Shanique Beckford on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Score_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
