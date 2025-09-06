6172a5a63a4869ee4a41e63836035e441bb7c7a7
Neikkie <neikkie21@outlook.com>
2025-09-06 00:29:49 -0400

    feat: added `@stateObject` comment

--- Score Counter/ContentView.swift
+++ Score Counter/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     // `@State` allows a variable within to struct to be changed
+    // `@StateObject` is a property wrapper that allows users to create a reference within a view that stays active to be used. www.hackingwithswift.com/quick-start/swiftui/what-is-the-stateobject-property-wrapper
+    
     @StateObject var viewModel: ScoreCounterViewModel
     
     var body: some View {
